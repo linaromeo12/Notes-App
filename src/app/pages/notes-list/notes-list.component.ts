c4ebb90bb869a011792d24935ce82fee48f406a1
linaromeo12 <lr4ej@virginia.edu>
2021-04-21 16:35:38 -0400

    Filter Functionality
    The notes app allows users to filter the notes based on an input

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -89,16 +89,61 @@ import { NotesService } from 'src/app/shared/notes.service';
 
 export class NotesListComponent implements OnInit {
   notes: Note[] = new Array<Note>(); 
+  filteredNotes: Note[] = new Array<Note>(); 
   
   constructor(private notesService: NotesService) { }
 
   ngOnInit(): void {
     //retrieve all notes from noteservice 
     this.notes = this.notesService.getAll(); // return all notes
-    
+    this.filteredNotes = this.notes; 
   }
   deleteNote(id: number){
     this.notesService.delete(id); 
+  }
+  filter(query: string){
+    query = query.toLowerCase().trim(); 
+
+    let allResults: Note[] = new Array<Note>(); 
+    //split up the search query into individual words
+    let terms: string[] = query.split(' '); //split on spaces
+    //remove duplicate 
+    terms = this.removeDuplicates(terms); 
+    //compile all relevent results into all results array 
+    terms.forEach(term => {
+      let results: Note[] = this.relevantNotes(term); 
+      //append results to the allresults array 
+      allResults = [...allResults, ...results]; 
+    }); 
+
+    //allResults will include duplicate notes because a term note can be the results of many matches 
+    // we have to remove duplicates 
+    let uniqueResults = this.removeDuplicates(allResults); 
+    this.filteredNotes = uniqueResults; 
+
+  }
+  removeDuplicates(arr: Array<any>){
+    let uniqueResults: Set<any> = new Set<any>(); 
+    // loop through the set 
+    arr.forEach(e => uniqueResults.add(e)); 
+
+    return Array.from(uniqueResults); 
+
+  }
+
+  relevantNotes(query: string): Array<Note>{
+    query = query.toLowerCase().trim(); 
+    let relevantNotes = this.notes.filter(note => {
+      if (note.title && note.title.toLowerCase().includes(query)){
+        return true; 
+      }
+      if (note.body && note.body.toLowerCase().includes(query)){
+        return true; 
+      }
+      return false; 
+    })
+
+    return relevantNotes; 
   }
 
 }
